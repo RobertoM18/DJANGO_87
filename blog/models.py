3c2bc0889d0fce80ebf545a0ab3fb5bcb8698d9e
unknown <GabrielTargaryen>
2024-05-14 23:31:08 -0500

    Pagina Cartas

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 class Post(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField()
-    image= CloudinaryField('image')
+    image= CloudinaryField('images')
     date = models.DateField(datetime.date.today)
     
 
