ba37fca47bd221683f0fa5c01bb7168276098f99
unknown <GabrielTargaryen>
2024-05-14 22:00:10 -0500

    Pagina Cartas

--- build.sh
+++ build.sh
@@ -6,6 +6,4 @@ pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
-python manage.py makemigrations --no input
-python manage.py migrate
 # Apply any outstanding database migrations
